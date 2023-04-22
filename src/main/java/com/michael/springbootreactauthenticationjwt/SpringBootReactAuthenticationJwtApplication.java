0c2ef5f77ca0b0e8ae229b78600006e37ec4b17e
michael <mchege78@gmail.com>
2023-04-22 13:55:59 +0300

    initial commit

---
+++ src/main/java/com/michael/springbootreactauthenticationjwt/SpringBootReactAuthenticationJwtApplication.java
@@ -0,0 +1,13 @@
+package com.michael.springbootreactauthenticationjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactAuthenticationJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactAuthenticationJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michael/springbootreactauthenticationjwt/SpringBootReactAuthenticationJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michael.springbootreactauthenticationjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootReactAuthenticationJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
