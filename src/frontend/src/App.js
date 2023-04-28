e7c419ff68468838bb674a9d85f290f3454396c1
micleah <mchege78@gmail.com>
2023-04-29 02:02:39 +0300

    Recreating front-end with Semantic ui

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,20 +2,23 @@ import { useState } from "react";
 import "./App.css";
 import LoginForm from "./components/LoginForm";
 import StudentPage from "./components/Studentpage/StudentPage";
+import { AuthProvider } from "./Utils/AuthContext";
 
 function App() {
   const [bearerToken, setBearerToken] = useState("");
 
   return (
-    <main>
-      <LoginForm setBearerToken={setBearerToken} />
+    <AuthProvider>
+      <main>
+        <LoginForm setBearerToken={setBearerToken} />
 
-      <div className="div">
-        {bearerToken !== "" ? <StudentPage token={bearerToken} /> : ""}
-        <div>{bearerToken}</div>
-        <div></div>
-      </div>
-    </main>
+        <div className="div">
+          {bearerToken !== "" ? <StudentPage token={bearerToken} /> : ""}
+          <div>{bearerToken}</div>
+          <div></div>
+        </div>
+      </main>
+    </AuthProvider>
   );
 }
 
